267cb31f04fe0f60391d7956852557f9f0e0d098
EvgeniiGurenkov <evg-gurenkov@yandex.ru>
2022-07-16 12:16:07 +0400

    3rd task

---
+++ 3.py
@@ -0,0 +1,21 @@
+#Задана последовательность натуральных чисел, завершающаяся числом 0. Требуется определить значение второго по величине элемента в этой последовательности, то есть элемента, который будет наибольшим, если из последовательности удалить наибольший элемент.
+#Пример:
+#1
+#7
+#9
+#0
+#Вывод:
+#7
+
+first_max = int(input())
+second_max = int(input())
+if first_max < second_max:
+    first_max, second_max = second_max, first_max
+element = int(input())
+while element != 0:
+    if element > first_max:
+        second_max, first_max = first_max, element
+    elif element > second_max:
+        second_max = element
+    element = int(input())
+print(second_max)
